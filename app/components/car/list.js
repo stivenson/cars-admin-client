e2cf08d8735e29c7bd7ece4030f27fca54924584
Stivenson <stivenson.rpm@gmail.com>
2017-03-05 01:06:20 -0500

    Styles

--- app/components/car/list.js
+++ app/components/car/list.js
@@ -19,7 +19,61 @@ export const CarList = {
         this.vm.fetchProducts().then(this.vm.products).then(()=>this.vm.working(false)).then(()=>m.redraw());
     },
     view(c,p){
-        return <div class="car-list">LISTA</div>;
+        return (
+            <div class="car-list">
+                <div class="row">
+
+                  <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class="thumbnail">
+                        <div class="cont-image-product">
+                            <img class="image-product" alt="image product 1" src="https://img.buzzfeed.com/buzzfeed-static/static/2016-03/4/14/enhanced/webdr05/grid-cell-20993-1457120411-7.jpg" />
+                        </div>
+                        <div class="caption text-center">
+                            <h4>Nombre producto</h4>
+                            <p class="price-product">$400.000</p>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class="thumbnail">
+                        <div class="cont-image-product">
+                            <img class="image-product" alt="image product 1" src="http://www.radioactiva.cl/files/2014/07/Comida-china-de-tallarines-con-carne-y-verduras.jpg" />
+                        </div>
+                        <div class="caption text-center">
+                            <h4>Nombre producto</h4>
+                            <p class="price-product">$400.000</p>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class="thumbnail">
+                        <div class="cont-image-product">
+                            <img class="image-product" alt="image product 1" src="https://img.buzzfeed.com/buzzfeed-static/static/2016-03/4/14/enhanced/webdr05/grid-cell-20993-1457120411-7.jpg" />
+                        </div>
+                        <div class="caption text-center">
+                            <h4>Nombre producto</h4>
+                            <p class="price-product">$400.000</p>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class="thumbnail">
+                        <div class="cont-image-product">
+                            <img class="image-product" alt="image product 1" src="https://img.buzzfeed.com/buzzfeed-static/static/2016-03/4/14/enhanced/webdr05/grid-cell-20993-1457120411-7.jpg" />
+                        </div>
+                        <div class="caption text-center">
+                            <h4>Nombre producto</h4>
+                            <p class="price-product">$400.000</p>
+                        </div>
+                    </div>
+                  </div>                                    
+
+                </div>
+            </div>
+        );
     }
 }
 
